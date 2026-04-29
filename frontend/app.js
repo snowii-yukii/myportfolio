ecc4ebc744ef8273b3f42ae69e3226ecf953e10f
Carmelino Jadulco <carmelinojaduloc@gmail.com>
2026-04-30 02:11:26 +0800

    removed folder frontend

--- frontend/app.js
+++
@@ -1,8 +0,0 @@
-document.querySelector('.card').array.forEach(card => {
-        card.addEventListener('mousemove', e => {
-            const rect = card.getBoundingClientRect();
-            
-            card.style.setProperty('--x', `${e.clientX - rect.left}px`);
-            card.style.setProperty('--y', `${e.clienty - rect.top}px`);
-        })
-});
